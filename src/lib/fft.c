e04ec5d9d71be51c89281af190432a8a9faf47de
Josh Jeppson <joshua.jeppson@usu.edu>
2021-12-06 22:18:53 -0700

    Headers for fft

--- src/lib/fft.c
+++ src/lib/fft.c
@@ -1,22 +1,48 @@
 #include "fft.h"
 
+#include <stdlib.h>
+
 double *
 fft(double * buffer, uint64_t size) {
-
+	complex * complexBuffer = malloc(sizeof(complex) * size);
+	uint64_t i = 0;
+	for (; i < size; i++) {
+		complexBuffer[i] = CMPLX(buffer[i], 0);
+	}
+	complex * freqComplex = fft_complex(complexBuffer, size);
+	free(complexBuffer);
+	double * fDomain = malloc(sizeof(double) * size);
+	for (i = 0; i < size; i++) {
+		fDomain[i] = cabs(freqComplex[i]);
+	}
+	free(freqComplex);
+	return fDomain;
 }
 
 double *
 ifft(double * buffer, uint64_t size) {
+	complex * complexBuffer = malloc(sizeof(complex) * size);
+	uint64_t i = 0;
+	for (; i < size; i++) {
+		complexBuffer[i] = CMPLX(buffer[i], 0);
+	}
+	complex * timeComplex = ifft_complex(complexBuffer, size);
+	free(complexBuffer);
+	double * tDomain = malloc(sizeof(double) * size);
+	for (i = 0; i < size; i++) {
+		tDomain[i] = cabs(timeComplex[i]);
+	}
+	free(timeComplex);
+	return tDomain;
+}
+
+complex *
+fft_complex(complex * buffer, uint64_t size) {
 
 }
 
 complex *
-fft(complex * buffer, uint64_t size) {
-
-}
-
-complex *
-ifft(complex * buffer, uint64_t size) {
+ifft_complex(complex * buffer, uint64_t size) {
 
 }
 
--- src/lib/fft.h
+++ src/lib/fft.h
@@ -1,13 +1,57 @@
+/**
+ * Tiny Fourier transform library for use in Nodesynth
+ *
+ * By Josh Jeppson
+ * */
+
 #ifndef NODESYNTH_FFT_H_INCLUDED
 #define NODESYNTH_FFT_H_INCLUDED
 
 #include <stdint.h>
 
+/**
+ * Computes the DFT (Discrete Fourier Transform) using the FFT (Fast Fourier Transform) algorithm,
+ * which is a divide and conquer algorithm of O(n) = n log(n). This wrapper takes a buffer in
+ * double precision floating point. Converts to complex type and calls fft_complex.
+ *
+ * @param buffer the buffer to transform into the frequency domain
+ * @param size the number of elements in the buffer
+ * @return the buffer transformed into frequency domain
+ * */
 double * fft(double * buffer, uint64_t size);
+
+/**
+ * Given the DFT (Discrete Fourier Transform), computes the inverse FFT, which is also an O(n) = n log(n)
+ * algorithm. This wrapper takes a buffer in double precision floating point. Converts into complex
+ * type and calls ifft_complex.
+ *
+ * @param buffer the buffer (in frequency domain) to transform into the time domain
+ * @param size the number of elements in the buffer
+ * @return the buffer transformed into time domain
+ * */
 double * ifft(double * buffer, uint64_t size);
 
 // Helper functions using the complex type
-complex * fft(complex * buffer, uint64_t size);
-complex * ifft(complex * buffer, uint64_t size);
+
+/**
+ * Computes the DFT (Discrete Fourier Transform) using the FFT (Fast Fourier Transform) algorithm,
+ * which is a divide and conquer algorithm of O(n) = n log(n). This takes elements of type complex
+ * and returns elements of type complex.
+ *
+ * @param buffer the buffer to transform into the frequency domain
+ * @param size the number of elements in the buffer
+ * @return the buffer transformed into frequency domain
+ * */
+complex * fft_complex(complex * buffer, uint64_t size);
+
+/**
+ * Given the DFT (Discrete Fourier Transform), computes the inverse FFT, which is also an O(n) = n log(n)
+ * algorithm. This takes elements of type complex and returns elements of type complex
+ *
+ * @param buffer the buffer (in frequency domain) to transform into the time domain
+ * @param size the number of elements in the buffer
+ * @return the buffer transformed into time domain
+ * */
+complex * ifft_complex(complex * buffer, uint64_t size);
 
 #endif // NODESYNTH_FFT_H_INCLUDED

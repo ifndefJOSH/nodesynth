62b5cc50e7571086df06a99643596e4ff3ee3bbf
Josh Jeppson <joshua.jeppson@usu.edu>
2021-12-06 22:50:10 -0700

    First implementation for FFT

--- src/lib/fft.c
+++ src/lib/fft.c
@@ -1,6 +1,15 @@
+/**
+ * Implementation for FFT and IFFT functions for Nodesynth's built-in FFT lib
+ *
+ * By Josh Jeppson
+ *
+ * Note: This took me a grand total of 26 lines in Python
+ * */
+
 #include "fft.h"
 
 #include <stdlib.h>
+#include <math.h>
 
 double *
 fft(double * buffer, uint64_t size) {
@@ -38,11 +47,69 @@ ifft(double * buffer, uint64_t size) {
 
 complex *
 fft_complex(complex * buffer, uint64_t size) {
-
+	// Create omega array for the first size primitive roots of unity
+	complex * omega = malloc(sizeof(complex) * size);
+	uint64_t i = 0;
+	for (; i < size; i++) {
+		omega[i] = CMPLX(
+			cos(2 * M_PI * i)
+			, sin(2 * M_PI * i)
+		);
+	}
+	complex * freqDomain = fft_helper(buffer, omega, size);
+	free(omega);
+	return freqDomain;
 }
 
 complex *
 ifft_complex(complex * buffer, uint64_t size) {
-
+	// For the inverse fft, the angle in the complex plane is inverted
+	// from that of the regular fft
+	complex * omega = malloc(sizeof(complex) * size);
+	for (; i < size; i++) {
+		omega[i] = CMPLX(
+			cos(2 * M_PI * i)
+			, -sin(2 * M_PI * i)
+		);
+	}
+	complex * timeDomain = fft_helper(buffer, omega, size);
+	free(omega);
+	// The IFFT must be reverse-scaled back down by n (the size)
+	for (i = 0; i < size; i++) {
+		timeDomain[i] /= size;
+	}
+	return timeDomain;
 }
+
+complex *
+fft_helper(complex * buffer, complex * omega, uint64_t size) {
+	complex * even = malloc(sizeof(complex) * size / 2);
+	complex * odd  = malloc(sizeof(complex) * size / 2);
+	complex * omegaHalf = malloc(sizeof(complex) * size / 2);
+	uint64_t i = 0;
+	for (; i < size / 2; i++) {
+		// Get the even and odd elements
+		even[i] = buffer[i * 2];
+		odd[i]  = buffer[i * 2 + 1];
+		// Square the elements in the first half of the omega array
+		omegaHalf[i] = omega[i] * omega[i];
+	}
+
+	complex * solution_even = fft_helper(even, omegaHalf, size / 2);
+	complex * solution_odd  = fft_helper(odd,  omegaHalf, size / 2);
+	// Free memory before we forget
+	free(even);
+	free(odd);
+	free(omegaHalf);
+	// Re-combine the even and odd solutions of the FFT
+	complex * solution = malloc(sizeof(complex) * size);
+	for (i = 0; i < size / 2; i++) {
+		solution[i] = solution_even[i] + omega[i] * solution_odd[i];
+		solution[i + (size / 2)] = solution_even[i] - omega[i] * solution_odd[i];
+	}
+	free(solution_even);
+	free(solution_odd);
+	return solution;
+}
+
 
--- src/lib/fft.h
+++ src/lib/fft.h
@@ -12,7 +12,8 @@
 /**
  * Computes the DFT (Discrete Fourier Transform) using the FFT (Fast Fourier Transform) algorithm,
  * which is a divide and conquer algorithm of O(n) = n log(n). This wrapper takes a buffer in
- * double precision floating point. Converts to complex type and calls fft_complex.
+ * double precision floating point. Converts to complex type and calls fft_complex. Requires
+ * the buffer size to be a power of 2.
  *
  * @param buffer the buffer to transform into the frequency domain
  * @param size the number of elements in the buffer
@@ -23,7 +24,7 @@ double * fft(double * buffer, uint64_t size);
 /**
  * Given the DFT (Discrete Fourier Transform), computes the inverse FFT, which is also an O(n) = n log(n)
  * algorithm. This wrapper takes a buffer in double precision floating point. Converts into complex
- * type and calls ifft_complex.
+ * type and calls ifft_complex. Requires the buffer size to be a power of 2.
  *
  * @param buffer the buffer (in frequency domain) to transform into the time domain
  * @param size the number of elements in the buffer
@@ -36,7 +37,7 @@ double * ifft(double * buffer, uint64_t size);
 /**
  * Computes the DFT (Discrete Fourier Transform) using the FFT (Fast Fourier Transform) algorithm,
  * which is a divide and conquer algorithm of O(n) = n log(n). This takes elements of type complex
- * and returns elements of type complex.
+ * and returns elements of type complex. Requires the buffer size to be a power of 2.
  *
  * @param buffer the buffer to transform into the frequency domain
  * @param size the number of elements in the buffer
@@ -46,12 +47,25 @@ complex * fft_complex(complex * buffer, uint64_t size);
 
 /**
  * Given the DFT (Discrete Fourier Transform), computes the inverse FFT, which is also an O(n) = n log(n)
- * algorithm. This takes elements of type complex and returns elements of type complex
+ * algorithm. This takes elements of type complex and returns elements of type complex. Requires the
+ * buffer size to be a power of 2.
  *
  * @param buffer the buffer (in frequency domain) to transform into the time domain
  * @param size the number of elements in the buffer
  * @return the buffer transformed into time domain
  * */
 complex * ifft_complex(complex * buffer, uint64_t size);
+
+/**
+ * Helper function that creates the actual FFT/IFFT. This function should probably not be used by
+ * the end user, as it should only be called by fft_complex and ifft_complex. Assumes that the
+ * buffer size is a power of 2. This function takes an omega array, which should contain
+ * the primitive roots of unity.
+ *
+ * @param buffer Buffer to convert either in or out of the frequency domain
+ * @param omega Omega array--contains primitive roots of unity
+ * @param size The size of both the buffer and omega array
+ * */
+complex * fft_helper(complex * buffer, complex * omega, uint64_t size);
 
 #endif // NODESYNTH_FFT_H_INCLUDED
